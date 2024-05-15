bdcd4d9f55cfc793a38db5afb79231fdfa27aaf0
stepan <bohqdeweo@gmail.com>
2024-05-15 21:47:11 +0300

    test 3

--- app/src/main/java/com/example/competition/MainActivity.java
+++ app/src/main/java/com/example/competition/MainActivity.java
@@ -9,7 +9,6 @@ import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
 public class MainActivity extends AppCompatActivity {
-    int test;//перевірка заливу 2
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
